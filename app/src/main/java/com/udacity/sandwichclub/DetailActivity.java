eabcf6f3a4b4d111a7726837337676e20a091823
Slupik <Slupik98@gmail.com>
2018-02-15 21:31:49 +0100

    1.2.0RTM feat: Initial polishing UI of DetailActivity

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -1,9 +1,11 @@
 package com.udacity.sandwichclub;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -12,15 +14,36 @@ import com.udacity.sandwichclub.utils.JsonUtils;
 
 import org.json.JSONException;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    @BindView(R.id.also_known_tv)
+    TextView tvAlsoKnow;
+
+    @BindView(R.id.ingredients_tv)
+    TextView tvIngredients;
+
+    @BindView(R.id.origin_tv)
+    TextView tvOrigin;
+
+    @BindView(R.id.description_tv)
+    TextView tvDescription;
+
+    @BindView(R.id.image_iv)
+    ImageView ivSandwichImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        ButterKnife.bind(this);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
 
@@ -70,6 +93,51 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI(Sandwich sandwich) {
+        tvDescription.setText(sandwich.getDescription());
+        tvOrigin.setText(sandwich.getPlaceOfOrigin());
+        tvAlsoKnow.setText(getAltNamesText(sandwich));
+        tvIngredients.setText(getIngredientsList(sandwich));
+        loadSandwichImage(sandwich);
+    }
 
+    private void loadSandwichImage(Sandwich sandwich) {
+        new ImageDownloaderTask()
+                .setCallback(new ImageDownloaderTask.Callback() {
+                            @Override
+                            public void downloadFinished(Bitmap bm, int statusCode) {
+                                if(bm!=null) {
+                                    ivSandwichImage.setImageBitmap(bm);
+                                }
+                            }
+
+                            @Override
+                            public void downloadFinished(Exception exception) {
+                                Toast.makeText(
+                                        DetailActivity.this,
+                                        getString(R.string.check_internet_connection),
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                .execute(sandwich.getDescription());
+    }
+
+    private String getAltNamesText(Sandwich sandwich) {
+        return listToString(sandwich.getAlsoKnownAs());
+    }
+
+    private String getIngredientsList(Sandwich sandwich) {
+        return listToString(sandwich.getIngredients());
+    }
+
+    private static String listToString(List<String> list) {
+        StringBuilder text = new StringBuilder();
+        for(String value:list) {
+            if(text.length()==0) {
+                text = new StringBuilder(value);
+            } else {
+                text.append(", ").append(value);
+            }
+        }
+        return text.toString();
     }
 }
